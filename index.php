14cca882c2d767950a0ec8cd715d425eef228946
Hartley Brody <hbrody@hubspot.com>
2012-09-28 00:46:40 -0400

    started working on 'The Loop'

--- index.php
+++ index.php
@@ -14,36 +14,33 @@
 
 get_header(); ?>
 
-		<div id="primary" class="content-area">
-			<div id="content" class="site-content" role="main">
-
 			<?php if ( have_posts() ) : ?>
-
-				<?php _s_content_nav( 'nav-above' ); ?>
-
-				<?php /* Start the Loop */ ?>
+                <?php $post_num = 0; ?>
 				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
-					?>
+                    <?php $post_num += 1; ?>
+                    <?php if ( $post_num == 1): // this is the latest post, display as banner ?>
+                        
+                        <h1><?php the_title(); ?></h1>
+                        
+                        
+                    <?php else: // this is not the first post, display as a tile ?>
+                        
+                        <?php
+                            /* Include the Post-Format-specific template for the content.
+                             * If you want to overload this in a child theme then include a file
+                             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                             */
+                            get_template_part( 'content', get_post_format() );
+                        ?>
+                    <?php endif; ?>
 
 				<?php endwhile; ?>
-
-				<?php _s_content_nav( 'nav-below' ); ?>
 
 			<?php elseif ( current_user_can( 'edit_posts' ) ) : ?>
 
 				<?php get_template_part( 'no-results', 'index' ); ?>
 
 			<?php endif; ?>
-
-			</div><!-- #content .site-content -->
-		</div><!-- #primary .content-area -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
