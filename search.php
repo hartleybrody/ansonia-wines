b5b1409ac212a7ddd9d704386463b281462ebf53
Hartley Brody <hbrody@hubspot.com>
2012-10-07 00:06:40 -0400

    a nice search interface, to top it off

--- search.php
+++ search.php
@@ -8,34 +8,65 @@
 
 get_header(); ?>
 
-		<section id="primary" class="content-area">
-			<div id="content" class="site-content" role="main">
+    
+        <div class="row clearfix">
+            <div class="span12" id="logo-banner">
+                <a href="<?php echo home_url(); ?>">
+                    <img src="/wp-content/themes/_s/img/logo.jpg" />
+                </a>
+            </div>
+        </div>
+        
+        <div class="row clearfix">
+            <div class="span4">
+                Search Results for: "<?php echo get_search_query(); ?>" 
+            </div>
+            <div class="span8" style="text-align: right">
+                Search again: 
+                <span style="position:relative; display:inline-block; top: 5px">
+                    <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+                        <input type="text" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', '_s' ); ?>" />
+                        <input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', '_s' ); ?>" style="position:relative; top:-5px"/>
+                    </form>
+                </span>
+            </div>
+        </div>
+        
+    <?php if (have_posts()) : ?>
+        <?php $post_num = 0; ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+            <?php $post_num += 1; ?>
+            <?php if ( $post_num % 3 == 1): ?>
+                <div class="row tiles">
+            <?php endif; ?>
+                <div class="span4 tile">
+                    <?php if (get_post_type() == "post"): // it's a blog article?>
+                        <a href="<?php the_permalink() ?>" >
+                            <img src="<?php echo get_post_meta( $post->ID, 'tile_banner', True ); ?>" class="tile-post-img hover-swap" />
+                        </a><br>
+                        <a href="<?php the_permalink() ?>" class="tile-title">
+                            <?php the_title(); ?>
+                        </a>   
+                    <?php else : // assume it's a page?> 
+                        <a href="<?php the_permalink() ?>" class="tile-title">
+                            <h1 style="text-align: center"><?php the_title(); ?> Page</h1>
+                        </a>
+                    <?php endif; ?>
+                </div>
+            <?php if ( $post_num % 3 == 0): ?>
+                </div> <!--.row.tiles-->
+            <?php endif; ?>
+            
+		<?php endwhile; ?>
 
-			<?php if ( have_posts() ) : ?>
+    <?php else : ?>
+        <div class="row">
+            <div class="span12" style="text-align: center">
+                <h1>Couldn't find anything...</h4>
+                <h2>Try searching again?</h2>
+            </div>
+        </div>
 
-				<header class="page-header">
-					<h1 class="page-title"><?php printf( __( 'Search Results for: %s', '_s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-				</header><!-- .page-header -->
+    <?php endif; ?>
 
-				<?php _s_content_nav( 'nav-above' ); ?>
-
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php get_template_part( 'content', 'search' ); ?>
-
-				<?php endwhile; ?>
-
-				<?php _s_content_nav( 'nav-below' ); ?>
-
-			<?php else : ?>
-
-				<?php get_template_part( 'no-results', 'search' ); ?>
-
-			<?php endif; ?>
-
-			</div><!-- #content .site-content -->
-		</section><!-- #primary .content-area -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- searchform.php
+++ searchform.php
@@ -7,7 +7,11 @@
  */
 ?>
 	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-		<label for="s" class="assistive-text"><?php _e( 'Search', '_s' ); ?></label>
+        <label for="s" class="assistive-text">
+            <h3>
+                <?php _e( 'Search', '_s' ); ?>
+            </h3>
+        </label>
 		<input type="text" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', '_s' ); ?>" />
 		<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', '_s' ); ?>" />
 	</form>
