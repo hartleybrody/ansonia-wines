f6566e5cef5ed821a8769c16ffe395d2cb6869f5
Hartley Brody <hbrody@hubspot.com>
2012-12-01 19:16:54 -0500

    some outer page structure

--- archive.php
+++ archive.php
@@ -10,75 +10,75 @@
 
 get_header(); ?>
 
-		<section id="primary" class="content-area">
-			<div id="content" class="site-content" role="main">
 
-			<?php if ( have_posts() ) : ?>
+    <?php if ( have_posts() ) : ?>
 
-				<header class="page-header">
-					<h1 class="page-title">
-						<?php
-							if ( is_category() ) {
-								printf( __( 'Category Archives: %s', '_s' ), '<span>' . single_cat_title( '', false ) . '</span>' );
+        <header class="page-header">
+            <h1 class="page-title">
+                <?php
+                    if ( is_category() ) {
+                        printf( __( 'Category Archives: %s', '_s' ), '<span>' . single_cat_title( '', false ) . '</span>' );
 
-							} elseif ( is_tag() ) {
-								printf( __( 'Tag Archives: %s', '_s' ), '<span>' . single_tag_title( '', false ) . '</span>' );
+                    } elseif ( is_tag() ) {
+                        printf( __( 'Tag Archives: %s', '_s' ), '<span>' . single_tag_title( '', false ) . '</span>' );
 
-							} elseif ( is_author() ) {
-								/* Queue the first post, that way we know
-								 * what author we're dealing with (if that is the case).
-								*/
-								the_post();
-								printf( __( 'Author Archives: %s', '_s' ), '<span class="vcard"><a class="url fn n" href="' . get_author_posts_url( get_the_author_meta( "ID" ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' );
-								/* Since we called the_post() above, we need to
-								 * rewind the loop back to the beginning that way
-								 * we can run the loop properly, in full.
-								 */
-								rewind_posts();
+                    } elseif ( is_author() ) {
+                        /* Queue the first post, that way we know
+                         * what author we're dealing with (if that is the case).
+                        */
+                        the_post();
+                        printf( __( 'Author Archives: %s', '_s' ), '<span class="vcard"><a class="url fn n" href="' . get_author_posts_url( get_the_author_meta( "ID" ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' );
+                        /* Since we called the_post() above, we need to
+                         * rewind the loop back to the beginning that way
+                         * we can run the loop properly, in full.
+                         */
+                        rewind_posts();
 
-							} elseif ( is_day() ) {
-								printf( __( 'Daily Archives: %s', '_s' ), '<span>' . get_the_date() . '</span>' );
+                    } elseif ( is_day() ) {
+                        printf( __( 'Daily Archives: %s', '_s' ), '<span>' . get_the_date() . '</span>' );
 
-							} elseif ( is_month() ) {
-								printf( __( 'Monthly Archives: %s', '_s' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
+                    } elseif ( is_month() ) {
+                        printf( __( 'Monthly Archives: %s', '_s' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
 
-							} elseif ( is_year() ) {
-								printf( __( 'Yearly Archives: %s', '_s' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
+                    } elseif ( is_year() ) {
+                        printf( __( 'Yearly Archives: %s', '_s' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
 
-							} else {
-								_e( 'Archives', '_s' );
+                    } else {
+                        _e( 'Archives', '_s' );
 
-							}
-						?>
-					</h1>
-					<?php
-						if ( is_category() ) {
-							// show an optional category description
-							$category_description = category_description();
-							if ( ! empty( $category_description ) )
-								echo apply_filters( 'category_archive_meta', '<div class="taxonomy-description">' . $category_description . '</div>' );
+                    }
+                ?>
+            </h1>
+            <?php
+                if ( is_category() ) {
+                    // show an optional category description
+                    $category_description = category_description();
+                    if ( ! empty( $category_description ) )
+                        echo apply_filters( 'category_archive_meta', '<div class="taxonomy-description">' . $category_description . '</div>' );
 
-						} elseif ( is_tag() ) {
-							// show an optional tag description
-							$tag_description = tag_description();
-							if ( ! empty( $tag_description ) )
-								echo apply_filters( 'tag_archive_meta', '<div class="taxonomy-description">' . $tag_description . '</div>' );
-						}
-					?>
-				</header><!-- .page-header -->
+                } elseif ( is_tag() ) {
+                    // show an optional tag description
+                    $tag_description = tag_description();
+                    if ( ! empty( $tag_description ) )
+                        echo apply_filters( 'tag_archive_meta', '<div class="taxonomy-description">' . $tag_description . '</div>' );
+                }
+            ?>
+        </header><!-- .page-header -->
 
-				<?php _s_content_nav( 'nav-above' ); ?>
+        <?php _s_content_nav( 'nav-above' ); ?>
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+        <div class="row">
+            <div class="span9">
+                <?php /* Start the Loop */ ?>
+                <?php while ( have_posts() ) : the_post(); ?>
 
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						// get_template_part( 'content', get_post_format() );
-					?>
+                    <?php
+                        /* Include the Post-Format-specific template for the content.
+                         * If you want to overload this in a child theme then include a file
+                         * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                         */
+                        // get_template_part( 'content', get_post_format() );
+                    ?>
                     
                     <div class="row excerpt">
                         <div class="span4">
@@ -86,7 +86,7 @@ get_header(); ?>
                                 <img src="<?php echo get_post_meta( $post->ID, 'tile_banner', True ); ?>" class="tile-post-img <?php if( get_post_meta( $post->ID, 'tile_banner_hover', True ) ){ echo "hover-swap"; } else{ echo "hover-fade"; }?>" />
                             </a>
                         </div><!--.span4-->
-                        <div class="span8">
+                        <div class="span5">
                             <div class="excerpt-header">
                                 <span class="categories"><?php 
                                 
@@ -121,18 +121,17 @@ get_header(); ?>
                         </div><!--.span4-->
                     </div><!--.row-->
 
-				<?php endwhile; ?>
+                <?php endwhile; ?>
+            </div><!--.span9-->
+            <div class="span3">
+                <?php get_sidebar(); ?>
+            </div>
+        </div>
+                
+    <?php else : ?>
 
-				<?php _s_content_nav( 'nav-below' ); ?>
+        <!-- no results -->
 
-			<?php else : ?>
+    <?php endif; ?>
 
-				<?php get_template_part( 'no-results', 'archive' ); ?>
-
-			<?php endif; ?>
-
-			</div><!-- #content .site-content -->
-		</section><!-- #primary .content-area -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
