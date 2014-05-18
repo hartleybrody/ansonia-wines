fdaf0a402b2fba56151625e75467ba53e56df726
Hartley Brody <hartley.brody@gmail.com>
2014-05-18 18:28:01 -0400

    forgot to update the search form as well

--- searchform.php
+++ searchform.php
@@ -7,11 +7,11 @@
  */
 ?>
 	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-        <h2 class="widgettitle clearfix">
-            <label for="s" class="assistive-text">
+        <label for="s" class="assistive-text">
+            <h2 class="widgettitle">
                 Search
-            </label>
-        </h2>
+            </h2>
+        </label>
 		<input type="text" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search posts&hellip;', '_s' ); ?>" />
 		<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', '_s' ); ?>" />
 	</form>
