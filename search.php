d3f5557bde17dec6cceee8d3b7fda85def47faff
Hartley Brody <hartley.brody@gmail.com>
2015-02-03 15:12:01 -0500

    update search results page to resemble index page

--- search.php
+++ search.php
@@ -8,47 +8,55 @@
 
 get_header(); ?>
         
-        <div class="row clearfix">
-            <div class="span4">
-                Search Results for: "<?php echo get_search_query(); ?>" 
-            </div>
-            <div class="span8" style="text-align: right">
-                Search again: 
-                <span style="position:relative; display:inline-block; top: 5px">
-                    <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-                        <input type="text" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', '_s' ); ?>" />
-                        <input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', '_s' ); ?>" style="position:relative; top:-5px"/>
-                    </form>
-                </span>
+        <div class="row">
+            <div class="span12" id="search-terms">
+                search results for: <span class="terms">"<?php echo get_search_query(); ?>"</span>
             </div>
         </div>
         
     <?php if (have_posts()) : ?>
-        <?php $post_num = 0; ?>
+
         <?php while ( have_posts() ) : the_post(); ?>
-            <?php $post_num += 1; ?>
-            <?php if ( $post_num % 3 == 1): ?>
-                <div class="row tiles">
-            <?php endif; ?>
-                <div class="span4 tile">
-                    <?php if (get_post_type() == "post"): // it's a blog article?>
+            <div class="row result">
+                <?php if (get_post_type() == "post"): // it's a blog article?>
+                    <div class="span4">
                         <a href="<?php the_permalink() ?>" >
-                            <img src="<?php echo get_post_meta( $post->ID, 'tile_banner', True ); ?>" class="tile-post-img hover-swap" />
-                        </a><br>
-                        <a href="<?php the_permalink() ?>" class="tile-title">
-                            <?php the_title(); ?>
-                        </a>   
-                    <?php else : // assume it's a page?> 
-                        <a href="<?php the_permalink() ?>" class="tile-title">
-                            <h1 style="text-align: center"><?php the_title(); ?> Page</h1>
+                            <img src="<?php echo get_post_meta( $post->ID, 'full_banner', True ); ?>" class="tile-post-img hover-swap" />
                         </a>
-                    <?php endif; ?>
-                </div>
-            <?php if ( $post_num % 3 == 0): ?>
-                </div> <!--.row.tiles-->
-            <?php endif; ?>
-            
-		<?php endwhile; ?>
+                    </div>
+                    <div class="span8">
+                        <div class="post-date">
+                            <span class="post-date">
+                                <?php echo get_the_date('l, F j, Y'); ?>
+                            </span>
+                        </div> <!--.post-date-->
+
+                        <div class="post-title">
+                            <a href="<?php the_permalink() ?>">
+                                <h3><?php the_title(); ?></h3>
+                            </a>
+                        </div> <!--.post-title-->
+
+                        <div class="post-excerpt">
+                            <p>
+                                <?php echo get_the_excerpt(); ?>
+                            </p>
+                        </div> <!--.post-excerpt-->
+
+                        <div class="post-more">
+                            <a href="<?php the_permalink() ?>">
+                                Read More &rarr;
+                            </a>
+                        </div> <!--.post-more-->
+                    </div>
+                <?php else : // assume it's a page?> 
+                    <a href="<?php the_permalink() ?>" class="tile-title">
+                        <h1 style="text-align: center"><?php the_title(); ?> Page</h1>
+                    </a>
+                <?php endif; ?>
+
+            </div>
+        <?php endwhile; ?>
 
     <?php else : ?>
         <div class="row">
@@ -59,5 +67,17 @@ get_header(); ?>
         </div>
 
     <?php endif; ?>
+
+        <div class="row">
+            <div class="span12" style="text-align: center;">
+                Search again: 
+                <span style="position:relative; display:inline-block; top: 5px">
+                    <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+                        <input type="text" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', '_s' ); ?>" />
+                        <input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', '_s' ); ?>" style="position:relative; top:-5px"/>
+                    </form>
+                </span>
+            </div>
+        </div>
 
 <?php get_footer(); ?>
