b827f8ee3dcc3e461953b008c45a9db8517371d2
Hartley Brody <hartley.brody@gmail.com>
2015-02-09 15:14:27 -0500

    don't hardcore site URL into vigneron link

--- sidebar-post.php
+++ sidebar-post.php
@@ -29,7 +29,7 @@
                             if($vignerons){
                                 echo '<li><span class="key">Vigneron:</span> <span class="value">';
                                 foreach ($vignerons as $vigneron) {
-                                    echo '<a href="http://www.ansoniawines.com/vigneron/' . $vigneron->slug . '/">';
+                                    echo '<a href="' . site_url() . '/vigneron/' . $vigneron->slug . '/">';
                                     echo $vigneron->name;
                                     echo '</a>';
                                 }
