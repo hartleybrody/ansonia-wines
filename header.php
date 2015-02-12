469aced4aaf3bcccd81eba0a31d1bb51780ccd6b
Hartley Brody <hartley.brody@gmail.com>
2015-02-11 19:31:02 -0500

    add deep links to twitter and instagram

--- header.php
+++ header.php
@@ -165,13 +165,13 @@
                             </li>
                             <li class="link">
                                 <div style="opacity: 0.6;">
-                                    <a href="http://instagram.com/AnsoniaWines" target="_blank">
+                                    <a href="instagram://user?username=ansoniawines" target="_blank">
                                         <img src="<?php echo get_site_url(); ?>/wp-content/themes/_s/img/icons/instagram.png" class="social-icon" />
                                     </a>
-                                    <a href="https://www.facebook.com/AnsoniaWines" target="_blank">
+                                    <a href="https://www.facebook.com/ansoniawines" target="_blank">
                                         <img src="<?php echo get_site_url(); ?>/wp-content/themes/_s/img/icons/facebook.png" class="social-icon" />
                                     </a>
-                                    <a href="https://twitter.com/ansoniawines" target="_blank">
+                                    <a href="twitter://user?screen_name=ansoniawines" target="_blank">
                                         <img src="<?php echo get_site_url(); ?>/wp-content/themes/_s/img/icons/twitter.png" class="social-icon" />
                                     </a>
                                     <a href="https://www.pinterest.com/ansoniawines/" target="_blank">
