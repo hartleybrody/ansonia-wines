3800bb9857f9dc92ff52e7730b2cd5517a6d4d6d
Hartley Brody <hartley.brody@gmail.com>
2015-02-11 19:33:31 -0500

    add favicon

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon1.ico" />
+    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
