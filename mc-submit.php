1f32f545d2169da7abee5d5a53cf325c8db5ccfd
Hartley Brody <hartley.brody@gmail.com>
2016-09-15 12:18:17 -0400

    add a working (but hard-coded) mailchimp API integration

---
+++ mc-submit.php
@@ -0,0 +1,55 @@
+<?php
+
+    error_reporting(E_ALL & ~E_NOTICE);
+
+    $API_KEY = "0c0a15bde5cc40d382dc0599e1fe4764-us10";
+    $API_ROOT = "https://us10.api.mailchimp.com/3.0";
+    $BASIC_AUTH = "hartley:" . $API_KEY; // note that the 'user' before the colon can be any string
+    $AUTH_TOKEN = "Basic " . base64_encode($BASIC_AUTH);
+
+    $LIST_ID = "c9624d5296";
+    $endpoint = $API_ROOT . "/lists/" . $LIST_ID . "/members";
+
+    date_default_timezone_set('UTC');
+    $now = date('Y-m-d H:i:s');
+
+    $postData = array(
+        'email_address' => 'hartley.brody+bar@gmail.com',
+        'status' => 'subscribed',
+        'merge_fields' => array(
+            'FNAME' => 'Hartley',
+            'MMERGE3' => '02139'  // zipcode
+        ),
+
+        'ip_signup' => $_SERVER['REMOTE_ADDR'],
+        'timestamp_signup' => $now,
+        'ip_opt' => $_SERVER['REMOTE_ADDR'],
+        'timestamp_opt' => $now,
+    );
+
+    // echo(json_encode($postData));
+
+    $ch = curl_init($endpoint);
+    curl_setopt_array($ch, array(
+        CURLOPT_POST => TRUE,
+        CURLOPT_RETURNTRANSFER => TRUE,
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: '. $AUTH_TOKEN,
+            'Content-Type: application/json'
+        ),
+        CURLOPT_POSTFIELDS => json_encode($postData)
+    ));
+
+    // Send the request
+    $response = curl_exec($ch);
+
+    // Check for errors
+    if($response === FALSE){
+        die(curl_error($ch));
+    }
+
+    // Decode the response
+    $responseData = json_decode($response, TRUE);
+    print_r($responseData);
+
+?>
